6f29052e5e305f27beb1dac033277dadb2a551ff
Askar Pulatov <master15994@gmail.com>
2023-04-04 20:54:52 +0300

    add port2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ErrorHandler } from '@angular/core';
+import { withNavigationErrorHandler } from '@angular/router';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first_angular';
+  inputValue = '';
+  title = 'Fisrt Step in Angular btch';
+  number = 99;
+  img =
+    'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-vert.png';
+  constructor() {
+    setTimeout(() => {
+      console.log('Time is over');
+      this.img =
+        'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-jaune.png';
+    }, 3000);
+  }
+
+  onInput(event?: any) {
+    console.log('Event', event);
+    this.inputValue = event.target.value;
+  }
+
+  onClick() {
+    console.log('Shit man');
+  }
 }
-class Number {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
+  declarations: [AppComponent, PostComponent, Post2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
