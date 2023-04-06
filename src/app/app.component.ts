eac3cdbd934a64f79189fad0a7e62644e6c6e249
Askar Pulatov <master15994@gmail.com>
2023-04-06 10:47:25 +0300

    finish 4 block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,5 @@ export interface Post {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  posts: Post[] = [
-    {
-      title: 'Как слетать "Марс" на туда-обратно без гроша в кармани ',
-      text: 'Проезд нужно будет оплатить на Марсе',
-      id: 1,
-    },
-    {
-      title: 'Избавление от деманов',
-      text: ' Хотите избавится от деманов - Звоните!',
-      id: 2,
-    },
-  ];
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
+  isVisible = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 import { FormsModule } from '@angular/forms';
+import { StyleDirective } from './directives/style.directive';
 
 @NgModule({
-  declarations: [AppComponent, PostFormComponent, PostComponent],
+  declarations: [AppComponent, StyleDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
